4fc7ce5b44170f468884a8de91be66309b16cbc0
NoahMeissner <108337767+NoahMeissner@users.noreply.github.com>
2023-10-24 18:47:02 +0200

    init

---
+++ main.py
@@ -0,0 +1,21 @@
+from langchain.chat_models import ChatOpenAI
+import openai
+import os
+from langchain.schema.messages import HumanMessage, SystemMessage
+openai.api_key = ""
+
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Who won the world series in 2020?"},
+        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        {"role": "user", "content": "Where was it played?"}
+    ]
+)
+
+
+
+
+
+
