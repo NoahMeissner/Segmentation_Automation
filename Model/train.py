9f95ca3eb259311b619f42dbb74b72471ee7424b
michael-khalfin <michaelk.writing@yahoo.com>
2023-10-25 12:18:03 +0200

    imaging

---
+++ Model/train.py
@@ -0,0 +1,21 @@
+import sys
+print(sys.path)
+sys.path.append("/Users/michael_khalfin/opt/anaconda3/bin/ultralytics")
+
+import os
+print(os.getcwd())
+sys.path.append(os.getcwd())
+
+from ultralytics import YOLO
+ 
+# Load the model.
+model = YOLO('yolov8n.pt')
+ 
+# Training.
+results = model.train(
+   data='config.yaml',
+   imgsz=1280,
+   epochs=50,
+   batch=8,
+   name='yolov8n_v8_50e'
+)
