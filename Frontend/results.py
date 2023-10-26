96b8b2e31e3a86cec6f0fd2b3f52d7dd6029feb6
michael-khalfin <michaelk.writing@yahoo.com>
2023-10-26 05:58:47 +0200

    results

--- Frontend/results.py
+++ Frontend/results.py
@@ -3,6 +3,7 @@ from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QPixmap
 from PyQt6.QtWidgets import QApplication, QMainWindow, QTextEdit, QPushButton, QVBoxLayout, QWidget, QLabel, QGridLayout
 import json
+from PIL import Image
 
 class TestApp(QMainWindow):
     def __init__(self):
@@ -19,18 +20,31 @@ class TestApp(QMainWindow):
 
         self.window = QWidget(self)
         self.setCentralWidget(self.window)
-        self.grid_layout = QGridLayout()
-        #self.accuracy_grid = QGridLayout()
+        self.grid_layout = QGridLayout(self.window)
+
         # Labels
         self.results_name = QLabel("results")
         self.pred_training_name = QLabel("Prediction Training")
         self.confusion_matrix_name = QLabel("Confusion Matrix")
         self.precision_recall_name = QLabel("Precision Recall")
+
+        def load_and_resize_image(image_path, xfactor, yfactor):
+            image = Image.open(image_path)
+            width = int(image.size[0] * xfactor)
+            height = int(image.size[1] * yfactor)
+            image = image.resize((width, height), Image.Resampling.LANCZOS)
+            pixmap = QPixmap.fromImage(image.toqimage())
+            return pixmap
+
         # Pictures
-        self.results = QPixmap(graphDirs["results"])
-        self.pred_training = QPixmap(graphDirs["pred training"])
-        self.confusion_matrix = QPixmap(graphDirs["confusion matrix"])
-        self.precision_recall = QPixmap(graphDirs["precision recall"])
+        results_path = "/Users/michael_khalfin/bosse/Model/runs/detect/"+graphDirs["results"]
+        self.results = load_and_resize_image(results_path, .2, .2)
+        pred_path = "/Users/michael_khalfin/bosse/Model/runs/detect/"+graphDirs["pred training"]
+        self.pred_training = load_and_resize_image(pred_path, .2, .2)
+        conf_path = "/Users/michael_khalfin/bosse/Model/runs/detect/"+graphDirs["confusion matrix"]
+        self.confusion_matrix = load_and_resize_image(conf_path, .2, .2)
+        #prec_path = "/Users/michael_khalfin/bosse/Model/runs/detect/"+graphDirs["precision recall"]
+        #self.precision_recall = load_and_resize_image(prec_path, .2, .2)
 
         # QLabel widgets to display images
         self.results_label = QLabel()
@@ -42,29 +56,27 @@ class TestApp(QMainWindow):
         self.confusion_matrix_label = QLabel()
         self.confusion_matrix_label.setPixmap(self.confusion_matrix)
 
-        self.precision_recall_label = QLabel()
-        self.precision_recall_label.setPixmap(self.precision_recall)
+        #self.precision_recall_label = QLabel()
+        #self.precision_recall_label.setPixmap(self.precision_recall)
 
         # Fill Grid
         self.grid_layout.addWidget(self.results_name, 0, 0)
-        self.grid_layout.addWidget(self.results, 1, 0)
+        self.grid_layout.addWidget(self.results_label, 1, 0)
 
         self.grid_layout.addWidget(self.pred_training_name, 0, 1)
-        self.grid_layout.addWidget(self.pred_training_name, 1, 1)
+        self.grid_layout.addWidget(self.pred_training_label, 1, 1)
 
         self.grid_layout.addWidget(self.confusion_matrix_name, 2, 0)
-        self.grid_layout.addWidget(self.confusion_matrix, 3, 0)
+        self.grid_layout.addWidget(self.confusion_matrix_label, 3, 0)
 
-        self.grid_layout.addWidget(self.precision_recall_name, 2, 1)
-        self.grid_layout.addWidget(self.precision_recall, 3, 1)
+        #self.grid_layout.addWidget(self.precision_recall_name, 2, 1)
+        #self.grid_layout.addWidget(self.precision_recall_label, 3, 1)
         self.window.setLayout(self.grid_layout)
 
 def main():
     app = QApplication([])
     Test = TestApp()
     Test.show()
-
-
     sys.exit(app.exec())
 
 if __name__ == '__main__':
